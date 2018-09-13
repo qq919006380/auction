ebc54549c65dac193e30fea2c5638b2e67f516b8
weibo <919006380@qq.com>
2018-09-13 23:41:39 +0800

    新增产品详情

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from '@angular/router'
+import { ActivatedRoute } from '@angular/router'
+import { ProductService, Product } from '../shared/product.service';
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
-  productTitle:string
-  constructor(private routeInfo:ActivatedRoute) { }
+  product: Product
+  constructor(
+    private routeInfo: ActivatedRoute,
+    private ProductService: ProductService
+  ) { }
 
   ngOnInit() {
-    this.productTitle=this.routeInfo.snapshot.params['prodTitle']
+    let productId: number = this.routeInfo.snapshot.params['productId']
+    this.product = this.ProductService.getProduct(productId)
   }
 
 }
